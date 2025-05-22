ad67ab005c0e0488b58ab495fbc1b5e37a2363db
Grigory Vladimiro <grigori.vladimiro@gmail.com>
2025-05-22 10:51:02 +0200

    Add voting functionality for movies

--- src/sistema_votaciones.py
+++ src/sistema_votaciones.py
@@ -8,3 +8,11 @@ def añadir_pelicula():
     else:
         peliculas[nombre] = 0
         print(f"Película '{nombre}' añadida con éxito.")
+
+def votar_pelicula():
+    nombre = input("Por qué película quieres votar? ").strip()
+    try:
+        peliculas[nombre] += 1
+        print(f"Has votado por '{nombre}'.")
+    except KeyError:
+        print("Error: La película no está registrada. Intenta otra.")
